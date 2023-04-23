90df8860563106f62c093a7b8b939c5e0a4bcf9a
Ryan Martin <rlmartin@gmail.com>
2023-04-23 08:36:23 -0400

    chore: publish to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,22 @@
 import { typescript } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
+
+const majorVersion = 0;
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'config-loader',
+  name: '@rlmartin/config-loader',
   projenrcTs: true,
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  majorVersion,
+  releaseBranches: {
+    dev: { prerelease: 'dev', npmDistTag: 'dev', majorVersion },
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'],
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
