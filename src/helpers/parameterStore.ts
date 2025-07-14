9038124da1f8343eaad33a95259bd9b2a33460d1
Ryan Martin <rlmartin@gmail.com>
2025-07-14 16:27:55 -0400

    fix: need this flag to support SecureString parameters

--- src/helpers/parameterStore.ts
+++ src/helpers/parameterStore.ts
@@ -1,4 +1,4 @@
-import { SSMClient, GetParametersCommand } from '@aws-sdk/client-ssm';
+import { SSMClient, GetParametersCommand, ParameterType } from '@aws-sdk/client-ssm';
 
 export async function getParameterValues<C>(parameterNames: (keyof C)[], region: string | undefined = undefined, prefix: string = ''): Promise<Partial<Record<keyof C, string | string[]>>> {
   const client = new SSMClient({
@@ -6,13 +6,14 @@ export async function getParameterValues<C>(parameterNames: (keyof C)[], region:
   });
   const input = {
     Names: parameterNames.map(name => `${prefix}${String(name)}`),
+    WithDecryption: true,
   };
   const response = await client.send(new GetParametersCommand(input));
   return (response.Parameters ?? []).reduce((result, param) => {
     if (param.Name) {
       const name = prefix === '' ? param.Name : param.Name.replace(prefix, '');
       const value = param.Value ?? '';
-      result[name] = param.DataType === 'StringList' ? value.split(',') : value;
+      result[name] = param.Type === ParameterType.STRING_LIST ? value.split(',') : value;
     }
     return result;
   }, Object.assign({}));
